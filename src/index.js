fc1963a1c4a4bd298300315c0746c96936204e17
Kartik Dhiman <kartikjuly@gmail.com>
2021-01-21 23:55:13 +0530

    build: configure webpack

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./style.scss";
+
+module.hot?.accept();
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  devServer: {
+    port: 9000,
+    contentBase: "./dist",
+    hot: true,
+  },
+};

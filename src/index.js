965c713cef4c03e338aef0b44b58b09e1c0a862e
Kartik Dhiman <kartikjuly@gmail.com>
2021-01-28 13:33:44 +0530

    feat: animated hamburger and overlay

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './styles/style.scss';
+
+const hamburger = document.querySelector('#hamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+hamburger.addEventListener('click', () => {
+  if (header.classList.contains('open')) {
+    header.classList.remove('open');
+    overlay.classList.add('fade-out');
+    overlay.classList.remove('fade-in');
+  } else {
+    header.classList.add('open');
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');
+  }
+});
